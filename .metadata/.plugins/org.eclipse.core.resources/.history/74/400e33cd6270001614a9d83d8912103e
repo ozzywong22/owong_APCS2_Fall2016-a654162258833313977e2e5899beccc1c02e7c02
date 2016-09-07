cf9d7d2cc9c13f54458bf0aaf3b0f81e1c25e542
ozzywong22 <18wongo28@millshighsch.org>
2016-09-06 19:08:03 -0700

    new repository
    plz work

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/74/400e33cd6270001614a9d83d8912103e
@@ -0,0 +1,8 @@
+//Oscar Wong 8/31/16
+public class DoMath {
+
+	public static void main(String[] args) {
+		Calculate.square(5);
+	}
+}
+
---
+++ Calculate Library/src/Calculate.java
@@ -0,0 +1,19 @@
+//Oscar Wong Calculate Library 8/31/26
+//square will square an integer that is given
+public class Calculate {
+	public static int square(int number){
+		return(number * number);
+	}
+	public static int cube(int number){
+		return(number * number * number);
+	}
+	public static double average(double numOne, double numTwo){
+		return((numOne + numTwo)/2);
+	}
+	//public static averageOverload(double num1, double num2, double num3){
+		return((num1 + num2 + num3)/3);
+	}
+	public static toDegrees(double number){
+		double x = 3.14159/80
+		return(x * number)
+}
